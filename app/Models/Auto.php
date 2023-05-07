20babdbafd200565d6e270e2b6e3f323f478fc8a
Valera-V <valera@dreamersofday.co>
2023-05-07 12:56:45 +0300

    Auto - Create model

---
+++ app/Models/Auto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Auto extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['make', 'model', 'price'];
+}
